ad5cc5f5f0c3d28df06dcc268e884aa70ec8fa70
Valdo <vlorian22@GMAIL.COM>
2020-12-05 15:25:26 -0600

    10.1.7

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,9 @@
+const { TestScheduler } = require('jest');
+const Potion = require('../lib/Potion');
+
+test ('creates a health potion object', ()=>{
+  const potion= new Potion('health');
+
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
--- __tests__/random.test.js
+++
@@ -1,6 +0,0 @@
-const { TestScheduler } = require('jest');
-const checkIfEqual= require('../lib/random.js');
-
-test('checks if 10 is equal to 10', ()=>{
- expect(checkIfEqual(10,10)).toBe(true);
-});
