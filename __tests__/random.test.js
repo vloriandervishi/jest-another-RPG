b8548c93ebd006817da01bdfe9e04cb1e19fb446
Valdo <vlorian22@GMAIL.COM>
2020-12-05 13:07:59 -0600

    created first test file

---
+++ __tests__/random.test.js
@@ -0,0 +1,9 @@
+const { TestScheduler } = require('jest');
+const checkIfEqual = require('../lib/random.js');
+
+test('checks if 10 is equal to 10', ()=>{
+ expect(checkIfEqual(10,10)).toBe(true);
+});
+module.export=function(){
+
+};
