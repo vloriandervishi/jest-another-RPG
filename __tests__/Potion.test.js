01195c888ef41c59de8c0a027f52798eda0f4c62
Valdo <vlorian22@GMAIL.COM>
2020-12-05 16:44:14 -0600

    finished 10.1 lesson

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -2,8 +2,9 @@ const { TestScheduler } = require('jest');
 const Potion = require('../lib/Potion');
 
 test ('creates a health potion object', ()=>{
-  const potion= new Potion('health');
+  const potion= new Potion();
 
-  expect(potion.name).toBe('health');
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
 });
